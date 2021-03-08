ddcea58256e31952a82151fc1e8cf1b4a646a7f8
ezsusmu <susan.mustafa@ericsson.com>
2021-03-08 21:18:59 +0000

    Removing unnecessary folder

--- demo/src/test/java/com/example/demo/repository/FirstMongoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface FirstMongoRepository extends MongoRepository<Entity, String> {
-        
-    
-}
